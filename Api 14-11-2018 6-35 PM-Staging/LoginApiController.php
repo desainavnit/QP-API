aa9ddc6e6a41fd3b293b1ea2aab462258bed1d9f
Navnit <desainavnit1992@gmail.com>
2018-11-14 18:38:27 +0530

    Api 14-11-2018 6-35 PM-Staging
    Api 14-11-2018 6-35 PM-Staging

---
+++ Api 14-11-2018 6-35 PM-Staging/LoginApiController.php
@@ -0,0 +1,339 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\APIController;
+use Illuminate\Http\Request;
+use App\Notifications\ForgotPassword;
+use App\Notifications\NewPassword;
+use App\User;
+use App\Models\NotificationUser;
+use Sentinel;
+use Activation;
+use Datatables;
+use DB;
+use Session;
+use Response;
+use Socialite;
+use Cartalyst\Sentinel\Checkpoints\ThrottlingException;
+
+class LoginApiController extends APIController {
+
+    public function login(Request $request) {
+
+        $email = trim($request->email);
+        $device_id = trim($request->device_id);
+        $device_type = trim($request->device_type);
+        if ($email == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'The email field is required.'
+            );
+            return $this->respondWithStatus($response);
+        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'The email must be a valid email address.'
+            );
+            return $this->respondWithStatus($response);
+        } else if (trim($request->password) == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'The password field is required.'
+            );
+            return $this->respondWithStatus($response);
+        } else if ($device_id == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide device id.'
+            );
+            return $this->respondWithStatus($response);
+        } else if ($device_type == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide device type.'
+            );
+            return $this->respondWithStatus($response);
+        } else {
+
+            try {
+                $params = $request->all();
+                $getUser = Sentinel::findByCredentials($params);
+                if ($getUser) {
+                    $checkctivated = Activation::completed($getUser);
+                    if ($checkctivated) {
+                        if (Sentinel::authenticate($params)) {
+                            $roles = Sentinel::findById($getUser->id)->roles[0];
+
+                            $user = User::find($getUser->id);
+                            $user->chat_active = 1;
+                            $user->save();
+
+//                            DB::table('chat_group_members')
+//                                    ->where("user_id", '=', $getUser->id)
+//                                    ->update(['email_sent' => 0]);
+
+                            $profile_image_url = "";
+                            if ($getUser->image_profile != null) {
+                                $profile_image_url = url($getUser->image_profile);
+                            } else {
+                                $profile_image_url = url("assets/img/download.png");
+                            }
+
+                            $user_info = array(
+                                'user_id' => $getUser->id,
+                                'email' => $getUser->email,
+                                'first_name' => $getUser->first_name,
+                                'last_name' => $getUser->last_name,
+                                'username' => $getUser->first_name . " " . $getUser->last_name,
+                                'profile_picture' => $profile_image_url,
+                                'user_role_id' => $roles->id,
+                                'user_role_name' => $roles->name
+                            );
+                            $notification_data_exist = DB::table('notification_users')
+                                    ->where('user_id', '=', $getUser->id)
+                                    ->where('device_id', '=', $device_id)
+                                    ->exists();
+                            if ($notification_data_exist == 0) {
+                                $ins_notification_data = new NotificationUser();
+                                $ins_notification_data->user_id = $getUser->id;
+                                $ins_notification_data->device_id = $device_id;
+                                $ins_notification_data->device_type = $device_type;
+                                $ins_notification_data->save();
+                            }
+
+
+                            $response = array(
+                                'status' => 'success',
+                                'message' => 'Login success.',
+                                'user_info' => $user_info,
+                            );
+                            return $this->respondWithStatus($response);
+                        } else {
+                            $response = array(
+                                'status' => 'failed',
+                                'message' => 'Wrong username or password.'
+                            );
+                            return $this->respondWithStatus($response);
+                        }
+                    } else {
+                        $response = array(
+                            'status' => 'failed',
+                            'message' => 'Your registration is still being reviewed.You will be notified by email once your application has been approved.'
+                        );
+                        return $this->respondWithStatus($response);
+                    }
+                } else {
+                    $response = array(
+                        'status' => 'failed',
+                        'message' => 'User not found.'
+                    );
+                    return $this->respondWithStatus($response);
+                }
+            } catch (ThrottlingException $e) {
+                $minutes = $e->getDelay() / 60 % 60;
+                $second = $e->getDelay() % 60;
+                return $this->respondWithStatus(['status' => "failed",
+                            'message' => 'Too many unsuccessful login attempts your account blocked for ' . $minutes . " Minutes " . $second . " Seconds"
+                ]);
+            }
+        }
+    }
+
+    function logout(Request $request) {
+        $user_id = $request->user_id;
+        $device_id = trim($request->device_id); 
+
+        if ($user_id == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide user id.'
+            );
+            return $this->respondWithStatus($response);
+        } else if ($device_id == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide device id.'
+            );
+            return $this->respondWithStatus($response);
+        } else {
+            $user = Sentinel::findById($user_id);
+            if (!empty($user)) {
+                if ($user) {
+                    $user->chat_active = 0;
+                    $user->save();
+                    DB::table('im_usersocket')->where('userid', '=', $user->id)->delete();
+                    DB::table('notification_users')->where('user_id', '=', $user->id)->where('device_id', '=', $device_id)->delete();
+                }
+                Sentinel::logout();
+                $response = array(
+                    'status' => 'success',
+                    'message' => 'Logout success'
+                );
+                return $this->respondWithStatus($response);
+            } else {
+                $response = array(
+                    'status' => 'failed',
+                    'message' => 'User id not found'
+                );
+                return $this->respondWithStatus($response);
+            }
+        }
+    }
+
+    public function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    function forgot_password(Request $request) {
+
+        $email = trim($request->email);
+        if ($email == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'The email field is required.'
+            );
+            return $this->respondWithStatus($response);
+        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'The email must be a valid email address.'
+            );
+            return $this->respondWithStatus($response);
+        } else {
+
+            $getUser = Sentinel::findByCredentials(['login' => $request->email]);
+            if ($getUser) {
+
+                $randString = $this->generateRandomString(6);
+                $user = User::find($getUser->id);
+                $user->reset_code = '';
+                $user->save();
+
+                Sentinel::update($getUser, ['password' => $randString]);
+                $user->notify(new NewPassword($user, $randString));
+                $response = array(
+                    'status' => 'success',
+                    'message' => 'New password has been sent; please check your email.'
+                );
+                return $this->respondWithStatus($response);
+            } else {
+                $response = array(
+                    'status' => 'failed',
+                    'message' => 'User not found.'
+                );
+                return $this->respondWithStatus($response);
+            }
+        }
+    }
+
+    public function linkedin_login(Request $request) {
+        $email = trim($request->email);
+        $linkedin_id = trim($request->linkedin_id);
+        if ($email == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide email id.'
+            );
+            return $this->respondWithStatus($response);
+        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'The email must be a valid email address.'
+            );
+            return $this->respondWithStatus($response);
+        } else if ($linkedin_id == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide linked in id.'
+            );
+            return $this->respondWithStatus($response);
+        }
+
+        try {
+
+            $user_data = User::where('email', '=', $email)
+//                    ->orWhere('linkedin_id', '=', $linkedin_id)
+                    ->first();
+            if ($user_data) {
+                $found_linkedin_data = User::where('linkedin_id', '=', $linkedin_id)->first();
+                if (!$found_linkedin_data) {
+                    User::where('id', $user_data->id)->update(array('linkedin_id' => $linkedin_id));
+                }
+                $getUser = Sentinel::findById($user_data->id);
+                $getRoles = Sentinel::findById($user_data->id)->roles()->first();
+
+                if ($getUser) {
+                    $checkctivated = Activation::completed($getUser);
+                    if ($checkctivated) {
+
+                        $user = User::find($getUser->id);
+                        $user->chat_active = 1;
+                        $user->save();
+
+                        DB::table('im_group_members')
+                                ->where("u_id", '=', $getUser->id)
+                                ->update(['email_sent' => 0]);
+
+                        $profile_image_url = "";
+                        if ($getUser->image_profile != null) {
+                            $profile_image_url = url($getUser->image_profile);
+                        } else {
+                            $profile_image_url = url("assets/img/download.png");
+                        }
+
+                        $user_info = array(
+                            'user_id' => $getUser->id,
+                            'email' => $getUser->email,
+                            'first_name' => $getUser->first_name,
+                            'last_name' => $getUser->last_name,
+                            'username' => $getUser->first_name . " " . $getUser->last_name,
+                            'profile_picture' => $profile_image_url,
+                            'user_role_id' => $getRoles->id,
+                            'user_role_name' => $getRoles->name
+                        );
+                        $response = array(
+                            'status' => 'success',
+                            'message' => 'Login success.',
+                            'user_info' => $user_info,
+                        );
+                        return $this->respondWithStatus($response);
+                    } else {
+                        $response = array(
+                            'status' => 'failed',
+                            'message' => 'Your Qualpros account is not yet activated, Please check your email to activate your account.if you have sign up as tutor and activated your account, Please wait for Qualpros to approve your tutor account.'
+                        );
+                        return $this->respondWithStatus($response);
+                    }
+                } else {
+                    $response = array(
+                        'status' => 'failed',
+                        'message' => 'User not found.'
+                    );
+                    return $this->respondWithStatus($response);
+                }
+            } else {
+                $response = array(
+                    'status' => 'failed',
+                    'message' => "I'm sorry we could not confirm your registration, in order to continue with LinkedIn registration and login please make sure you use the same email address associated with your LinkedIn account."
+                );
+                return $this->respondWithStatus($response);
+            }
+        } catch (Exception $e) {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Error something went wrong.'
+            );
+            return $this->respondWithStatus($response);
+        }
+    }
+
+}
+
+?>
---
+++ Api 14-11-2018 6-35 PM-Staging/PaymentApiController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\APIController;
+use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\PrivateTuition;
+use App\Models\User;
+use Response;
+use DB;
+use App\Notifications\StudentPaymentPrivateTuition;
+use App\Notifications\TutorPaymentPrivateTuition;
+
+class PaymentApiController extends APIController {
+
+    function store_stripe_paymet_response(Request $request) {
+        $payment_type = $request->payment_type;
+        $student_id = $request->student_id;
+
+        if ($student_id == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide student id.',
+            );
+            return $this->respondWithStatus($response);
+        }
+        if ($payment_type == "") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide payment type.',
+            );
+            return $this->respondWithStatus($response);
+        }
+        if ($payment_type != "private" && $payment_type != "order") {
+            $response = array(
+                'status' => 'failed',
+                'message' => 'Please provide valid payment type.',
+            );
+            return $this->respondWithStatus($response);
+        }
+        if ($payment_type == "private") {
+            $tution_id = $request->tution_id;
+            if ($tution_id == "") {
+                $response = array(
+                    'status' => 'failed',
+                    'message' => 'Please provide tution id.',
+                );
+                return $this->respondWithStatus($response);
+            } else {
+                $this->PrivateTuitionemail($student_id, $tution_id);
+                $private_tution_order_data = PrivateTuition::where('user_id', $student_id)
+                                ->whereIn('id', explode(",", $tution_id))
+                                ->where([
+                                    ['status', '1'],
+                                    ['payment_status', '0']
+                                ])->get();
+                if (count($private_tution_order_data) == 0) {
+                    $response = array(
+                        'status' => 'failed',
+                        'message' => 'Tution data not found.',
+                    );
+                    return $this->respondWithStatus($response);
+                }
+                foreach ($private_tution_order_data as $key => $orders) {
+                    $tutor_scribblar_id = $orders->tutor->scribblar_id;
+                    $student_scribblar_id = $orders->student->scribblar_id;
+                    $usernameTutor = $orders->tutor->getFullName();
+
+                    if ($tutor_scribblar_id == NULL) {
+                        $first_nameTutor = $orders->tutor->first_name;
+                        $last_nameTutor = $orders->tutor->last_name;
+                        $emailTutor = $orders->tutor->email;
+                        $tutor_scribblar_id = register_scribblar_account($usernameTutor, $first_nameTutor, $last_nameTutor, $emailTutor);
+                        $updateUser = User::where('id', $orders->tutor->id)->first();
+                        $updateUser->scribblar_id = @$tutor_scribblar_id;
+                        $updateUser->update();
+                    }
+
+                    if ($student_scribblar_id == NULL) {
+                        $usernameS = $orders->student->getFullName();
+                        $first_nameS = $orders->student->first_name;
+                        $last_nameS = $orders->student->last_name;
+                        $emailS = $orders->student->email;
+                        $student_scribblar_id = register_scribblar_account($usernameS, $first_nameS, $last_nameS, $emailS);
+                        $updateUserS = User::where('id', $orders->student->id)->first();
+                        $updateUserS->scribblar_id = @$student_scribblar_id;
+                        $updateUserS->update();
+                    }
+
+                    $course_name = 'Private Tuition ' . $usernameTutor . ' Date : ';
+                    $lesson_title = $orders->start_date . ' - ' . str_random(5);
+                    $dataClassRoom = get_classroom_scribblar($course_name, $lesson_title, $tutor_scribblar_id);
+
+                    $classroom_id = $dataClassRoom['classroom_id'];
+                    $token_tutor = $dataClassRoom['token_id'];
+                    $token_student = generate_token_scribblar();
+
+// $orders->update(['payment_status'=>1],['token_tutor'=>$token_tutor],['token_student'=>$token_student],['classroom_id'=>$classroom_id]); //already comment this line in strippayment controller
+                    $orders->price_per_h = $orders->tutor->price_per_h;
+                    $orders->payment_status = 1;
+                    $orders->token_tutor = $token_tutor;
+                    $orders->token_student = $token_student;
+                    $orders->classroom_id = $classroom_id;
+                    $orders->save();
+
+                    $sum_amount = $orders->tutor->price_per_h * $orders->duration;
+//                    $date_payment = date('Y-m-d H:i:s', $charge['created']);
+                    $date_payment = date('Y-m-d H:i:s');
+                    $sum_amount = $orders->tutor->price_per_h * $orders->duration;
+//                    Order::where('tuition_id', $orders->id)->update(['status' => 1, 'date_payment' => $date_payment, 'payment_amount' => $sum_amount, 'payer_info' => $transaction_data]);
+                    Order::where('tuition_id', $orders->id)->update(['status' => 1, 'date_payment' => $date_payment, 'payment_amount' => $sum_amount, 'payer_info' => 'test']);
+                }
+                $response = array(
+                    'status' => 'success',
+                    'message' => 'Payment received successfully.',
+                );
+                return $this->respondWithStatus($response);
+            }
+        } else if ($payment_type == "order") {
+            $order_data = Order::where('student_id', $student_id)->where('status', '=', 0)
+                            ->join('courses', 'orders.course_id', '=', 'courses.id')->wherenull('courses.deleted_at');
+           
+        }
+    }
+
+    /*
+      Send Email Private Tuition
+     */
+
+    public function PrivateTuitionemail($student_id, $tution_id) {
+        $userStudent = User::where('id', $student_id)->first();
+        $privatetuition = PrivateTuition::where('user_id', $student_id)
+                ->whereIn('id', explode(",", $tution_id))
+                ->where('status', 1)
+                ->where('payment_status', '0')
+                ->get();
+        $url = url('student/dashboard');
+        $urlTutor = url('tutor/private-tuition');
+        $userStudent->notify(new StudentPaymentPrivateTuition($userStudent, $privatetuition, $url));
+        foreach ($privatetuition as $key => $value) {
+            $tutor = User::where('id', $value->tutor_id)->first();
+            $tutor->notify(new TutorPaymentPrivateTuition($tutor, $value, $urlTutor));
+        }
+    }
+
+}
